b0a6fc283045cb55516686c54845f7afbb9b76c3
Sophimp <songfangxi2012@163.com>
2021-05-26 09:43:33 +0800

    update push.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,10 @@
-git remote add github git@github.com:sophimp/blogs.git
-git push origin master
-git push github master
+#!/bin/bash
+function obtain_git_branch {
+  br=`git branch | grep "*"`
+  echo ${br/* /}
+}
+br=`obtain_git_branch`
+echo Current git branch is $br
+git remote add github git@github.com:sophimp/vim.git
+git push origin $br
+git push github $br
